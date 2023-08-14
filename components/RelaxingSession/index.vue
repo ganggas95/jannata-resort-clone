0be2b50bd5636a7c1fd755d1d2d377e4e0c43e6d
ganggas <subhannizar25@gmail.com>
2023-08-14 15:23:26 +0800

    Initialize relaxing session

---
+++ components/RelaxingSession/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const sectionElement = ref<HTMLElement | null>(null);
+</script>
+<template>
+  <section class="relaxing-session" id="relaxing-session" ref="sectionElement">
+    <div class="container">
+        
+    </div>
+  </section>
+</template>
