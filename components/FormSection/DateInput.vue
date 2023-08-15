da3fcd59a828b1f07afc94762554989eb8a73af3
ganggas <subhannizar25@gmail.com>
2023-08-15 10:29:56 +0800

    CU-860rhfjd2 - As a site visitor, I will be able to see check-in and checkout form

---
+++ components/FormSection/DateInput.vue
@@ -0,0 +1,143 @@
+<script setup lang="ts">
+import IconDots from "./IconDots.vue";
+const inputEl = ref<HTMLInputElement | null>(null);
+const emit = defineEmits(["update:modelValue"]);
+
+const props = defineProps<{
+  type?: string;
+  placeholder: string;
+  name: string;
+  modelValue: string | number;
+  max?: number | string;
+  min?: number | string;
+}>();
+const isNumberInput = computed(() => props.type === "number");
+const isDateInput = computed(() => props.type === "date");
+
+const inputValue = computed({
+  get(): string | number {
+    return props.modelValue;
+  },
+  set(value: number | string) {
+    emit("update:modelValue", value);
+  },
+});
+
+const valueExceedMax = computed(
+  () => props.max && Number(inputValue.value) >= Number(props.max)
+);
+const valueExceedMin = computed(
+  () => props.min && Number(inputValue.value) <= Number(props.min)
+);
+
+const increaseValue = () => {
+  if (typeof inputValue.value === "number" && !valueExceedMax.value) {
+    inputValue.value += 1;
+  }
+};
+const decreaseValue = () => {
+  if (props.type === "number")
+    if (typeof inputValue.value === "number" && !valueExceedMin.value) {
+      inputValue.value -= 1;
+    }
+};
+
+const toggleCalendar = () => {
+  inputEl.value?.showPicker();
+};
+</script>
+<template>
+  <div
+    class="field-wrapper"
+    :class="{
+      'field-wrapper__number': isNumberInput,
+      'field-wrapper__date': isDateInput,
+    }"
+  >
+    <input
+      :type="type"
+      :name="name"
+      :placeholder="placeholder"
+      :max="max"
+      :min="min"
+      v-model="inputValue"
+      ref="inputEl"
+    />
+    <IconDots v-if="isDateInput" @click="toggleCalendar" />
+    <span class="input-addon left" v-if="isNumberInput" @click="decreaseValue">
+      &#8722;
+    </span>
+    <span class="input-addon right" v-if="isNumberInput" @click="increaseValue">
+      +
+    </span>
+  </div>
+</template>
+<style lang="scss" setup>
+.field-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  position: relative;
+  border: 1px solid #679334;
+  padding: 15px;
+  box-sizing: border-box;
+  input {
+    width: 100%;
+    height: 100%;
+    border: none;
+    outline: none;
+    background-color: transparent;
+    color: #679334;
+    letter-spacing: 1px;
+    text-align: center;
+  }
+  span.input-addon {
+    width: 20px;
+    position: absolute;
+    color: #fff;
+    font-size: 20px;
+    cursor: pointer;
+    user-select: none;
+    text-align: center;
+    &.left {
+      left: 10% !important;
+      &::before {
+        content: "";
+      }
+    }
+    &.right {
+      right: 10% !important;
+    }
+  }
+}
+input[type="date"]::-webkit-calendar-picker-indicator {
+  display: none;
+}
+
+/* For Firefox */
+input[type="date"] {
+  -moz-appearance: none;
+}
+
+/* For Edge */
+input[type="date"]::-ms-clear {
+  display: none;
+}
+input[type="number"]::-webkit-inner-spin-button,
+input[type="number"]::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+input[type="number"] {
+  -moz-appearance: textfield;
+}
+
+input[type="number"]::-moz-number-spinbox,
+input[type="number"]::-moz-number-spin-up,
+input[type="number"]::-moz-number-spin-down {
+  display: none;
+}
+</style>
---
+++ components/FormSection/index.vue
@@ -0,0 +1,196 @@
+<script lang="ts" setup>
+import DateInput from "./DateInput.vue";
+const book = reactive({
+  checkin: "",
+  checkout: "",
+  adults: 1,
+  children: 1,
+});
+const today = computed(() => new Date().toISOString().split("T")[0]);
+</script>
+<template>
+  <section class="form-section">
+    <div class="bg-overlay">
+      <div class="container">
+        <form class="form-section__content">
+          <div class="form-group">
+            <DateInput
+              type="date"
+              name="checkin"
+              placeholder="Check-in"
+              v-model="book.checkin"
+              :min="today"
+            />
+            <DateInput
+              type="date"
+              name="checkin"
+              placeholder="Check-out"
+              v-model="book.checkout"
+              :min="today"
+            />
+          </div>
+          <div class="form-group__member">
+            <div class="form-control">
+              <label for="adult">Adult(s)</label>
+              <DateInput
+                type="number"
+                name="adult"
+                id="adult"
+                :min="1"
+                placeholder="Adult"
+                v-model="book.adults"
+              />
+            </div>
+            <div class="form-control">
+              <label for="children">Children</label>
+              <DateInput
+                type="number"
+                name="children"
+                id="children"
+                :min="1"
+                placeholder="Children"
+                v-model="book.children"
+              />
+            </div>
+          </div>
+          <div class="form-action">
+            <button class="form-book__btn">Rates & Availability</button>
+            <div class="form-discover">
+              <a class="form-discover__link">Discover Our Exclusive Deals</a>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </section>
+</template>
+<style lang="scss" scoped>
+.form-section {
+  height: 100vh;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-image: url("/bg-form.jpeg");
+
+  .bg-overlay {
+    background-color: rgba(0, 0, 0, 0.5);
+    height: 100%;
+    width: 100%;
+    position: absolute;
+  }
+  .container {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    height: 100%;
+  }
+  .form-section__content {
+    display: flex;
+    flex-direction: column;
+    row-gap: 1rem;
+
+    .form-group {
+      display: flex;
+      column-gap: 1rem;
+    }
+    .form-group__member {
+      display: flex;
+      flex-direction: column;
+      row-gap: 1rem;
+    }
+    .form-discover {
+      display: flex;
+      justify-content: center;
+    }
+    .form-control {
+      display: flex;
+      row-gap: 1px;
+      flex-direction: column;
+      label {
+        color: #fff;
+        text-transform: uppercase;
+        letter-spacing: 1px;
+        font-size: 12px;
+      }
+    }
+    button.form-book__btn {
+      background-color: #679334;
+      color: #fff;
+      padding: 14px 15px;
+      font-size: 12px;
+      letter-spacing: 1px;
+      text-transform: uppercase;
+      box-sizing: border-box;
+      margin-top: 1rem;
+      &:hover {
+        background-color: #fff;
+        color: #679334;
+      }
+    }
+    a.form-discover__link {
+      color: #fff;
+      padding: 14px 15px;
+      font-size: 12px;
+      letter-spacing: 1px;
+      text-transform: uppercase;
+      text-align: center;
+      position: relative;
+      cursor: pointer;
+      text-decoration: none;
+      box-sizing: border-box;
+      overflow: hidden;
+      margin-top: 1rem;
+
+      &::after {
+        content: "";
+        width: 0;
+        background-color: #679334;
+        height: 1px;
+        transition: transform 0.3s ease-in-out;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        transform: translateX(-100%);
+        will-change: transform;
+      }
+
+      &:hover {
+        color: #679334;
+        &::after {
+          width: 100%;
+          transform: translateX(0);
+        }
+      }
+    }
+    .form-action {
+      display: flex;
+      flex-direction: row;
+      column-gap: 1rem;
+      width: 100%;
+      position: relative;
+    }
+
+    @media not all and (max-width: 767px) {
+      max-width: 450px !important;
+      margin-right: 10rem;
+      .form-action {
+        button,
+        div {
+          width: 50%;
+        }
+      }
+
+      .form-group__member {
+        flex-direction: row;
+        column-gap: 1rem;
+        .form-control {
+          width: 50%;
+        }
+      }
+      .form-discover {
+        display: flex;
+      }
+    }
+  }
+}
+</style>
